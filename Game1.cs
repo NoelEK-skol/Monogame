9d1d09ec876b7ffbf25510f8d444bff80d79857b
NoelEK-skol <noek07001@utb.vaxjo.se>
2024-11-23 14:03:31 +0100

    Lagt till poäng system. mm

--- Game1.cs
+++ Game1.cs
@@ -11,6 +11,7 @@ public class Game1 : Game
 
     Texture2D pixel;
     SpriteFont fontScore;
+    SpriteFont Vinst;
 
     Rectangle paddleLeft = new Rectangle(10, 200, 20, 100);
     Rectangle paddleRight = new Rectangle(770, 200, 20, 100);
@@ -21,6 +22,8 @@ public class Game1 : Game
 
     int scoreLeftPlayer = 0;
     int scoreRightPlayer = 0;
+    bool gameOver = false;
+    string strWin = "";
 
     public Game1()
     {
@@ -43,46 +46,72 @@ public class Game1 : Game
         // TODO: use this.Content to load your game content here
         pixel = Content.Load<Texture2D>("Pixel");
         fontScore = Content.Load<SpriteFont>("Score");
+        Vinst = Content.Load<SpriteFont>("Vinst");
     }
 
     protected override void Update(GameTime gameTime)
     {
         if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
             Exit();
-
-        KeyboardState kState = Keyboard.GetState();
-        if (kState.IsKeyDown(Keys.W) && paddleLeft.Y > 0){
-            paddleLeft.Y-=8;
-        }
-        if (kState.IsKeyDown(Keys.S) && paddleLeft.Y + paddleLeft.Height < 480){
-            paddleLeft.Y+=8;
-        }
-
-        if (kState.IsKeyDown(Keys.Up) && paddleRight.Y > 0){
-            paddleRight.Y-=8;
-        }
-         if (kState.IsKeyDown(Keys.Down) && paddleRight.Y + paddleRight.Height < 480){
-            paddleRight.Y+=8;
-        }
-
-        ball.X+= (int)velocityX;
-        ball.Y+= (int)velocityY;
-        if(ball.Intersects(paddleRight) || 
-            ball.Intersects(paddleLeft)){
-            velocityX *= -1.1f;
-            velocityY *= 1.1f;
-        }
-
-        if(ball.Y <= 0 || ball.Y + ball.Height >= 480){
-            velocityY *= -1;
-        }
         
-        if(ball.X <= 0 || ball.X + ball.Width >= 800){
-            ball.X = 390;
-            ball.Y = 230;
-            velocityX = 3;
-            velocityY = 2;
+        if(!gameOver){
+
+            KeyboardState kState = Keyboard.GetState();
+            if (kState.IsKeyDown(Keys.W) && paddleLeft.Y > 0){
+                paddleLeft.Y-=8;
+            }
+            if (kState.IsKeyDown(Keys.S) && paddleLeft.Y + paddleLeft.Height < 480){
+                paddleLeft.Y+=8;
+            }
+
+            if (kState.IsKeyDown(Keys.Up) && paddleRight.Y > 0){
+                paddleRight.Y-=8;
+            }
+            if (kState.IsKeyDown(Keys.Down) && paddleRight.Y + paddleRight.Height < 480){
+                paddleRight.Y+=8;
+            }
+
+            ball.X+= (int)velocityX;
+            ball.Y+= (int)velocityY;
+            if(ball.Intersects(paddleRight) || 
+                ball.Intersects(paddleLeft)){
+                velocityX *= -1.1f;
+                velocityY *= 1.1f;
+            }
+
+            if(ball.Y <= 0 || ball.Y + ball.Height >= 480){
+                velocityY *= -1;
+            }
+            
+            if(ball.X <= 0){
+                ball.X = 390;
+                ball.Y = 230;
+                velocityX = 3;
+                velocityY = 2;
+                scoreRightPlayer ++;
+            }
+
+            if(ball.X + ball.Width >= 800){
+                ball.X = 390;
+                ball.Y = 230;
+                velocityX = 3;
+                velocityY = 2;
+                scoreLeftPlayer ++;
+            }
+            if(scoreLeftPlayer == 10){
+                scoreLeftPlayer = 0;
+                scoreRightPlayer = 0;
+                strWin = "Vanster Vann!";
+                gameOver = true;
+            }
+            if(scoreRightPlayer == 10){
+                scoreLeftPlayer = 0;
+                scoreRightPlayer = 0;
+                strWin = "Hoger Vann!";
+                gameOver = true;
+            }
         }
+
 
         // TODO: Add your update logic here
 
@@ -95,8 +124,11 @@ public class Game1 : Game
 
         // TODO: Add your drawing code here
         _spriteBatch.Begin();
-        _spriteBatch.DrawString(fontScore, scoreLeftPlayer.ToString(), new Vector2(10,10), Color.DarkOrange);
-        _spriteBatch.DrawString(fontScore, scoreRightPlayer.ToString(), new Vector2(730,10), Color.DarkOrange);
+        _spriteBatch.DrawString(fontScore, scoreLeftPlayer.ToString(), new Vector2(30,10), Color.DarkOrange);
+        _spriteBatch.DrawString(fontScore, scoreRightPlayer.ToString(), new Vector2(710,10), Color.DarkOrange);
+        if(gameOver){
+            _spriteBatch.DrawString(Vinst, strWin, new Vector2(200,40), Color.Black);
+        }
 
         _spriteBatch.Draw(pixel,paddleLeft,Color.HotPink); //ritar ut figurer och ändrar färg
         _spriteBatch.Draw(pixel,paddleRight,Color.HotPink);
